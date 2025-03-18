8e75dfa04ca6a2911f1f8e3c998dd9fa41599172
stephwinam <stephaniewinam@gmail.com>
2025-03-18 16:30:24 +0300

    Added the operation calculation

--- calc.py
+++ calc.py
@@ -1,3 +1,18 @@
-num1 = (input("Enter the first number: "))
-num2 = (input("Enter the second number: "))
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
 operation = input("Enter an operation (+, -, *, /): ")
+
+if operation == '+':
+    print(num1, "+", num2, "=", num1 + num2)
+elif operation == '-':
+    print(num1, "-", num2, "=", num1 - num2)
+elif operation == '*':
+    print(num1, "*", num2, "=", num1 * num2)
+elif operation == '/':
+    if num2 == 0:
+        print("Error: Cannot divide by zero. Please enter a nonzero number")
+    else:
+        print(num1 / num2)
+else:
+    print("Invalid operation")
+
